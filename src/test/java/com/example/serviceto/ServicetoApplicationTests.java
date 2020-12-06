59248d7df99d569ff96f1aa2f5c203a86aa3c729
laserany <dotachessfan@outlook.com>
2020-12-06 09:31:21 -0600

    Initial config-client properties

---
+++ src/test/java/com/example/serviceto/ServicetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serviceto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
